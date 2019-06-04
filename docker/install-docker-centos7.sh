a61f3d678fe7d19e222cef0e8a57f421afeb12cd
sdh <xbb123@naver.com>
2019-06-04 18:04:31 +0900

    add

---
+++ docker/install-docker-centos7.sh
@@ -0,0 +1,14 @@
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+      --add-repo \
+      https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
